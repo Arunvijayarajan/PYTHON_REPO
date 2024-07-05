fb23b22586c5f92a6c40cb7333fff624e42ce32a
Arunvijayarajan <arunvijayarajan9096@gmail.com>
2024-07-04 23:04:02 -0700

    trigonometric Function

---
+++ Trigonometric function/math.asin(X),matha.cos(x),math.tana(X).py
@@ -0,0 +1,8 @@
+import math
+result_asin=math.degrees(math.asin(0.5))
+result_acos=math.degrees(math.acos(0.5))
+result_atan=math.degrees(math.atan(1))
+print(result_acos)
+print(result_asin)
+print(result_atan)
+
