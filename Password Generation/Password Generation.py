d876cc66435187b730151eb69341979545daeece
Arunvijayarajan <arunvijayarajan9096@gmail.com>
2024-07-21 21:48:41 -0700

    Password Generator

---
+++ Password Generation/Password Generation.py
@@ -0,0 +1,9 @@
+import random
+lower="abcdefghijklmnopqrstuvwxyz"
+upper="ABCDFGHIJKLMNOPQRSTUVWXYZ"
+numbers="0123456789"
+symbols="!@#$%^&*()_-+=?><["
+all_chars=lower+upper+numbers+symbols
+length=int(input("Enter a length:"))
+password=''.join(random.sample(all_chars, length))
+print("Generated Password:",password)
