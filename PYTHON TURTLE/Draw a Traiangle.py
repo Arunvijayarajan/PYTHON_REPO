0e30c706b579c4894ef80be6a748ae0be27493e7
Arunvijayarajan <arunvijayarajan9096@gmail.com>
2024-07-30 23:04:56 -0700

    PYTHON TURTLE

---
+++ PYTHON TURTLE/Draw a Traiangle.py
@@ -0,0 +1,14 @@
+import turtle
+screen=turtle.Screen()
+screen.bgcolor("white")
+screen.title("Draw a Triangle")
+triangle_turtle=turtle.Turtle()
+triangle_turtle.color("red")
+triangle_turtle.speed(1)
+def draw_triangle(size):
+    for _ in range(3):
+     triangle_turtle.forward(size)
+     triangle_turtle.left(120)
+draw_triangle(100)
+screen.mainloop()
+
